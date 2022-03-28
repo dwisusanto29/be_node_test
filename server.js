7859965ad6839170e93404bbf9ffb3ae30a36cb6
dwisusanto29 <dw.susanto@gmail.com>
2022-03-28 10:45:50 +0700

    Register users

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const http = require('http');
-const socketIO = require('socket.io')(http);
 const app = express();
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
@@ -23,14 +22,11 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/images", express.static('./images'));
-
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the API." });
 });
 require('./src/routes/auth.routes')(app);
-require('./src/routes/public.routes')(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
@@ -38,15 +34,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-socketIO.on('connection', (socket) => {
-  console.log('New client connected');
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-  socket.on('chat', (data) => {
-    console.log(data);
-    socketIO.emit('chat', data);
-  });
-});
 
 
--- src/models/index.js
+++ src/models/index.js
@@ -22,37 +22,5 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.category = require("../models/category.model")(sequelize, Sequelize);
 db.user = require("../models/user.model")(sequelize, Sequelize);
-
-db.merchant = require("../models/merchant.model")(sequelize, Sequelize);
-db.merchant_image = require("../models/merchant_image.model")(sequelize, Sequelize);
-db.merchant_content = require("../models/merchant_content.model")(sequelize, Sequelize);
-db.merchant_rate = require("../models/valet_rate.model")(sequelize, Sequelize);
-db.user_merchant = require("../models/user_merchant.model")(sequelize, Sequelize);
-db.user_level = require("../models/user_level")(sequelize, Sequelize);
-
-//Valet
-db.valet_order = require("../models/valet_order.model")(sequelize, Sequelize);
-db.valet_destination = require("../models/valet_destination.model")(sequelize, Sequelize);
-db.valet_history = require("../models/valet_history.model")(sequelize, Sequelize);
-db.coupon = require("../models/valet_coupon.model")(sequelize, Sequelize);
-db.status = require("../models/valet_status.model")(sequelize, Sequelize);
-
-//Homepage
-db.ad = require('../models/ad.model')(sequelize, Sequelize);
-
-//Relation page
-db.category.hasMany(db.merchant, { foreignKey: 'category_id' });
-db.merchant.hasMany(db.merchant_image, { foreignKey: 'merchant_id', as: 'slider' });
-db.merchant.hasMany(db.merchant_content, { foreignKey: 'merchant_id', as: 'contents' });
-db.merchant.hasMany(db.merchant_rate, { foreignKey: 'merchant_id', as: 'merchant_rates' });
-db.merchant_image.belongsTo(db.merchant, { foreignKey: 'merchant_id', as: 'merchant' });
-db.merchant_content.belongsTo(db.merchant, { foreignKey: 'merchant_id', as: 'merchant' });
-db.user.hasMany(db.user_merchant, { foreignKey: 'user_id', as: 'merchant' });
-// db.merchant.hasMany(db.user_merchant, { foreignKey: 'merchant_id' });
-
-db.user_merchant.belongsTo(db.user, { foreignKey: 'user_id', as: 'user' });
-db.user_merchant.belongsTo(db.merchant, { foreignKey: 'merchant_id' });
-
 module.exports = db;
