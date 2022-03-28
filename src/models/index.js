13a774ae765cd21e987be53cb2ac1c3efea8a45c
dwisusanto29 <dw.susanto@gmail.com>
2022-03-28 10:50:37 +0700

    Model Created

--- src/models/index.js
+++ src/models/index.js
@@ -23,4 +23,7 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.user = require("../models/user.model")(sequelize, Sequelize);
+db.wallet = require("../models/wallet.model")(sequelize, Sequelize);
+db.walletTrans = require("../models/walletTrans.model")(sequelize, Sequelize);
+
 module.exports = db;
